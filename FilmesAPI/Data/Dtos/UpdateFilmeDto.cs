e7218a7afbd945f8ef7cbbf0dfa1178bc43c842e
Greice Felipe <greice.n.felipe@gmail.com>
2022-01-31 10:04:10 -0300

    Primeira API com C#

---
+++ FilmesAPI/Data/Dtos/UpdateFilmeDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class UpdateFilmeDto
+    {
+        [Required(ErrorMessage = "O campo titulo é obrigatório.")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo diretor é obrigatório.")]
+        public string Diretor { get; set; }
+
+        [StringLength(30, ErrorMessage = "O genero deve ter tamanho máx 30")]
+        public string Genero { get; set; }
+
+        [Range(1, 600, ErrorMessage = "A duração deve ter no min 1 e no máx 600 min.")]
+        public int Duracao { get; set; }
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Key]
+        [Required]
+        public int Id { get; internal set; }
+
+        [Required(ErrorMessage = "O campo titulo é obrigatório.")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo diretor é obrigatório.")]
+        public string Diretor { get; set; }
+
+        [StringLength(30, ErrorMessage = "O genero deve ter tamanho máx 30")]
+        public string Genero { get; set; }
+
+        [Range(1,600, ErrorMessage = "A duração deve ter no min 1 e no máx 600 min.")]
+        public int Duracao { get; set; }
+    }
+}
